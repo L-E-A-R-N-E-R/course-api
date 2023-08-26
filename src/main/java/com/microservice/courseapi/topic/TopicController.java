f98d84de370e7039dc60e858171ef1cff509ef0b
L-E-A-R-N-E-R <porwalmohit1999@gmail.com>
2023-08-25 23:16:56 -0700

    Creating a business service and separating the controller from business logic

--- src/main/java/com/microservice/courseapi/topic/TopicController.java
+++ src/main/java/com/microservice/courseapi/topic/TopicController.java
@@ -1,19 +1,18 @@
 package com.microservice.courseapi.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import java.util.Arrays;
 
 @RestController
 public class TopicController {
     
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
-        return Arrays.asList(
-            new Topic("1","Spring","Everything about Spring Framework"),
-            new Topic("2","Microservices","Microservice architecture and deploymeny"),
-            new Topic("3","Cloud","AWS")
-        );
+        return topicService.getAllTopics();
     }
 }
---
+++ src/main/java/com/microservice/courseapi/topic/TopicService.java
@@ -0,0 +1,20 @@
+package com.microservice.courseapi.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    List<Topic> topics = Arrays.asList(
+    new Topic("1","Spring","Everything about Spring Framework"),
+    new Topic("2","Microservices","Microservice architecture and deploymeny"),
+    new Topic("3","Cloud","AWS")
+    );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+}
